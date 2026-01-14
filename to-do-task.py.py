ab29025c1b12e259ad073db7d00347e10e2154f8
KennethSurti <surtikenneth@gmail.com>
2026-01-14 12:03:49 +0530

    initail commit

---
+++ to-do-task.py.py
@@ -0,0 +1,19 @@
+#this is the beginning of the task 
+#lets go
+
+
+
+
+
+import tkinter as tk
+from tkinter import messagebox
+import sqlite3
+
+
+
+
+#Priority Levels: Add a dropdown to mark tasks as Low, Medium, or High priority.
+
+#Due Dates: Use a calendar widget to set deadlines.
+
+#Search Bar: Add a filter to find specific tasks in a long list.
