d8a6857b6c9553e3a205d0eb7c1fa7d325028af5
KennethSurti <surtikenneth@gmail.com>
2026-02-01 02:38:25 +0530

    UI layout for calc

--- codsoft_3.py
+++ codsoft_3.py
@@ -12,3 +12,65 @@ app = ctk.CTk()
 app.title("Simple Calculator")
 app.geometry("420x420")
 app.resizable(False, False)
+
+
+
+# UI layout using the same customtikinter as before
+
+
+ctk.CTkLabel(
+    app,
+    text="Simple Calculator",
+    font=("Segoe UI", 24, "bold")
+).pack(pady=15)
+
+#Input system 
+
+num1_entry = ctk.CTkEntry(
+    app,
+    width=300,
+    placeholder_text="Enter first number"
+)
+num1_entry.pack(pady=8)
+
+num2_entry = ctk.CTkEntry(
+    app,
+    width=300,
+    placeholder_text="Enter second number"
+)
+num2_entry.pack(pady=8)
+
+
+# Operation System
+
+operation_var = ctk.StringVar(value="Select Operation")
+operation_menu = ctk.CTkOptionMenu(
+    app,
+    variable=operation_var,
+    values=[
+        "Addition (+)",
+        "Subtraction (-)",
+        "Multiplication (×)",
+        "Division (÷)"
+    ]
+)
+operation_menu.pack(pady=12)
+
+
+# The buttons (a little complex)
+
+ctk.CTkButton(
+    app,
+    text="Calculate",
+    command=calculate
+).pack(pady=15)
+
+result_entry = ctk.CTkEntry(
+    app,
+    width=300,
+    state="readonly",
+    placeholder_text="Result"
+)
+result_entry.pack(pady=10)
+
+app.mainloop()
