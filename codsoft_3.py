09a7f4265ac976e41b5bd40525dc877d4ba30448
KennethSurti <surtikenneth@gmail.com>
2026-02-01 02:19:51 +0530

    theme update (same as others)

--- codsoft_3.py
+++ codsoft_3.py
@@ -1,2 +1,14 @@
 # Inital start to the 3rd task 
 #  Going wiht making a calculator 
+
+import customtkinter as ctk
+from tkinter import messagebox
+
+# Theme of UI (using same as other tasks)
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+app = ctk.CTk()
+app.title("Simple Calculator")
+app.geometry("420x420")
+app.resizable(False, False)
